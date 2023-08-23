36f8bacf7014e9d77b379e1215028bbffb959d46
Francisco Alvarado <102932362+falvarado7@users.noreply.github.com>
2023-08-23 17:18:52 -0500

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -3,10 +3,21 @@ function findAuthorById(authors, id) {
   return foundId
 }
 
+/* Old function with no helper
 function findBookById(books, id) {
   let foundId = books.find((book) => book.id === id)
   return foundId
 }
+*/
+//New function using helper findElementById
+function findBookById(books, id) {
+  return findElementById(books, id);
+}
+// Helper function here
+function findElementById(elements, id) {
+  return elements.find((element) => element.id === id);
+}
+
 
 function partitionBooksByBorrowedStatus(books) {
    let booksCheckedIn = books.filter((book) =>
